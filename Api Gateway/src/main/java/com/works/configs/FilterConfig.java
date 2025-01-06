7ebd620460b55dad3e5b5d15cf2ee1d7271911fe
blthll <39004523+blthll@users.noreply.github.com>
2025-01-06 21:39:48 +0300

    Add files via upload

---
+++ Api Gateway/src/main/java/com/works/configs/FilterConfig.java
@@ -0,0 +1,52 @@
+package com.works.configs;
+
+import brave.Tracer;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+@Configuration
+public class FilterConfig implements Filter {
+
+    final Tracer tracer;
+    public FilterConfig(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        String spanId = tracer.currentSpan().context().spanIdString();
+        String traceId = tracer.currentSpan().context().traceIdString();
+
+        String url = req.getRequestURI();
+        String sessionID = req.getSession().getId();
+        String header = req.getHeader("user-agent");
+        String name = "";
+        String roles = "";
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            name = auth.getName();
+            roles = auth.getAuthorities().toString();
+        }
+
+        String time = ""+System.currentTimeMillis();
+
+        System.out.println(url + "-" + sessionID + "-" + header + "-" + name + "-"+roles+"-"+time);
+
+        res.setHeader("spanId", spanId);
+        res.setHeader("traceId", traceId);
+
+        filterChain.doFilter(req, res);
+    }
+
+}
---
+++ Customer/src/main/java/com/works/CustomerRestController.java
@@ -0,0 +1,88 @@
+package com.works;
+
+import com.works.models.Comment;
+import com.works.models.ProductModel;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("customer")
+public class CustomerRestController {
+
+    final IProduct iProduct;
+    final DiscoveryClient discoveryClient;
+    final IPlaceHolder iPlaceHolder;
+    final CircuitBreakerFactory circuitBreakerFactory;
+    final CircuitBreakerFactory globalCustomConfiguration;
+
+    public CustomerRestController(IProduct iProduct, DiscoveryClient discoveryClient, IPlaceHolder iPlaceHolder, CircuitBreakerFactory circuitBreakerFactory, CircuitBreakerFactory globalCustomConfiguration) {
+        this.iProduct = iProduct;
+        this.discoveryClient = discoveryClient;
+        this.iPlaceHolder = iPlaceHolder;
+        this.circuitBreakerFactory = circuitBreakerFactory;
+        this.globalCustomConfiguration = globalCustomConfiguration;
+    }
+
+    @GetMapping("data")
+    public Map data() {
+        Map map = new LinkedHashMap();
+        map.put("name", "Kemal");
+        map.put("surname", "Bilmem");
+        map.put("email", "kemal@gmail.com");
+        map.put("age", 22);
+        return map;
+    }
+
+    @GetMapping("product")
+    public Map product(@RequestParam(defaultValue = "0") int pageNumber) {
+        Map map = new LinkedHashMap();
+        ProductModel productModel = iProduct.productList(pageNumber);
+        map.put("data", productModel.getContent());
+        return map;
+        /*
+        RestTemplate restTemplate = new RestTemplate();
+        List<ServiceInstance> instances = discoveryClient.getInstances("product");
+        if (!instances.isEmpty()) {
+            ServiceInstance instance = instances.get(0);
+            String url = instance.getUri().toString() + "/product/list?pageNumber="+pageNumber;
+            ResponseEntity<ProductModel> response = restTemplate.getForEntity(url, ProductModel.class);
+            map.put("url", url);
+            map.put("data", response.getBody().getContent());
+        }
+        return map;
+         */
+    }
+
+    @GetMapping("comment")
+    public Comment comment(@RequestParam(defaultValue = "1") String id) {
+        CircuitBreaker breaker = circuitBreakerFactory.create("breaker1");
+        CircuitBreaker breaker1 = globalCustomConfiguration.create("breaker2");
+        return breaker.run(
+                () -> iPlaceHolder.getComments(id),
+                throwable -> fallBack(id)
+        );
+    }
+
+    private Comment fallBack(String id) {
+        Comment comment = new Comment();
+        comment.setId(0);
+        comment.setComment("Sample Comment");
+        comment.setPostId(1);
+        comment.setUserId(1);
+        return comment;
+    }
+
+}
